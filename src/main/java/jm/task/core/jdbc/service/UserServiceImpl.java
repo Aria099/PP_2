aec5c1cf5e2353babb0d215cf6c6d9735f8433b9
Aria <aria_099@mail.ru>
2025-02-03 20:31:05 +0300

    Initial commit

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -15,25 +15,17 @@ public class UserServiceImpl implements UserService {
     public void createUsersTable() { userDao.createUsersTable(); }
 
     @Override
-    public void dropUsersTable() {
-        userDao.dropUsersTable();
-    }
+    public void dropUsersTable() { userDao.dropUsersTable(); }
 
     @Override
     public void saveUser(String name, String lastName, byte age) { userDao.saveUser(name, lastName, age); }
 
     @Override
-    public void removeUserById(long id) {
-        userDao.removeUserById(id);
-    }
+    public void removeUserById(long id) { userDao.removeUserById(id); }
 
     @Override
-    public List<User> getAllUsers() {
-        return userDao.getAllUsers();
-    }
+    public List<User> getAllUsers() { return userDao.getAllUsers(); }
 
     @Override
-    public void cleanUsersTable() {
-        userDao.cleanUsersTable();
-    }
+    public void cleanUsersTable() { userDao.cleanUsersTable(); }
 }
